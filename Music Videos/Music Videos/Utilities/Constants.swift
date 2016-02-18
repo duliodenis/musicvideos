da17165114f9c1017c9a25e341435e6bfe17eb65
Dulio Denis <dulio.denis@yahoo.com>
2016-02-18 15:33:56 -0500

    Added Apple Reachability API in ObjC with a Bridging Header

--- Music Videos/Music Videos/Utilities/Constants.swift
+++ Music Videos/Music Videos/Utilities/Constants.swift
@@ -13,3 +13,12 @@ typealias JSONDictionary = [String: AnyObject]
 typealias JSONArray = Array<AnyObject>
 
 let iTunesTopMusicVideos = "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json"
+
+
+// Reachability Constants
+
+let WIFI = "WIFI Available"
+
+let NOACCESS = "No Internet Access"
+
+let WWAN = "Cellular Access Available"
